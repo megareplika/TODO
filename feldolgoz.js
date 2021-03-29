435a50038ec4116e79eb199f722e32df2f1d3d25
megareplika <megareplika@gmail.com>
2021-03-29 11:05:28 +0200

    js send to php

--- feldolgoz.js
+++ feldolgoz.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
     $("#add").click(bezur);
+    $("#elemlista").delegate("li .torol", torol);
+    $("#elemlista").delegate("li .kesz", torol);
 });
 
 
@@ -13,12 +15,34 @@ function bezur() {
 
 function listaba(cim, datum) {
     var HtmlLista = "<li>"
-    HtmlLista+= "<div>"+cim+"</div><div>"+datum+"</div><div><input type=\"image\" src = \"kuka.jpg\" alt=\"kuka\"> <input type=\"image\" src = \"pipa.jpg\" alt=\" pipa\"></div>";
-    HtmlLista+="</li>";
+    HtmlLista += "<div>" + cim + "</div><div>" + datum + "</div><div><input class =\"torol\" type=\"image\" src = \"kuka.jpg\" alt=\"kuka\"> <input class =\"kesz\" type=\"image\" src = \"pipa.jpg\" alt=\" pipa\"></div>";
+    HtmlLista += "</li>";
     $("#elemlista").append(HtmlLista);
 }
 
 function adatbazisba(cim, datum) {
-    $.ajax();
-    
+    $.ajax({
+        type: 'GET',
+        url: "feldolgoz.php?nev=" + cim + "datum=" + datum,
+        success: function (result) {
+            alert("sikeres beszuras");
+        },
+        error: function () {
+            alert("Nemsikerült felvinni");
+        }
+    });
+}
+
+function torol() {
+    let cim = $("#todonev").val();
+    $.ajax({
+        type: 'GET',
+        url: "feldolgoz.php?nev=" + cim,
+        success: function (result) {
+            alert("sikeres törlés");
+        },
+        error: function () {
+            alert("Nemsikerült a törlés");
+        }
+    });
 }
--- modosit.php
+++ modosit.php
@@ -1,3 +1,10 @@
 <?php
 
+include_once './MySqlDB.php';
+
+$db = new MySqlDB();
+$tablaNeve = "todo";
+$szuro = "todo ="+$_GET["nev"];
+
+$db ->torol($tablaNeve, $szuro);
 ?>
